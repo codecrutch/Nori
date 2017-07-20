cfb0e0cbaaf2adbd2f7654b650a344e98dd3e618
Sean M. Snyder <sean.snyder@snhu.edu>
2017-07-20 14:14:04 -0400

    Add spinner

--- frontend/components/search/search_result_index.jsx
+++ frontend/components/search/search_result_index.jsx
@@ -21,9 +21,14 @@ const mapDispatchToProps = (dispatch) => {
 class SearchResultIndex extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      loading: true
+    };
   }
 
   componentDidMount(){
+    setTimeout(() => this.setState({ loading: false }), 200); 
     document.body.scrollTop = 0;
     let search = this.props.location.search;
     if (search) {
@@ -50,15 +55,20 @@ class SearchResultIndex extends React.Component {
   displayBusinesses(){
     let businesses = this.props.businesses;
     if (businesses.length === 0) {
-      return <h1>No results</h1>;
+      return null;
     } else {
-      return businesses = businesses.map(business => <SearchResultListing className='business-listing' key={uniqueId('business')} business={business} />);
+      return businesses.map(business => <SearchResultListing className='business-listing' key={uniqueId('business')} business={business} />);
     }
   }
 
   render(){
-    return (
-      <section className="container">
+    const { loading } = this.state;
+    
+    let businessResults;
+    if(loading) {
+      businessResults = null;// render null when app is not ready
+    } else {
+      businessResults = (
         <div className="search-results row">
           <section className="business-results col-lg-5 col-xs-12 col-sm-12 col-md-6">
             {this.displayBusinesses()}
@@ -67,6 +77,12 @@ class SearchResultIndex extends React.Component {
             <MultiMap businesses={this.props.businesses} />
           </section>
         </div>
+      )
+    }
+
+    return (
+      <section className="container loader">
+        {businessResults}
       </section>
     );
   }
